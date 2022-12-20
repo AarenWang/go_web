27433b2b6cdc0754ae1cd0b5966b0a3cb30e26c6
zeke <zeke@nhtx.net>
2022-12-20 17:12:11 +0800

    11

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # go_web
+
+## 目前center和client两个模块是可以跑的
+
+## center的启动参数
+```shell
+-consul-addr="consul的地址" -id="" -ip=""
+```
+
+## client的启动参数
+```shell
+-center-addr="center的地址"
+```
--- center/main.go
+++ center/main.go
@@ -12,14 +12,14 @@ import (
 )
 
 func main() {
-	cip := flag.String("consul-ip", "127.0.0.1", "server agent ip address")
-	cport := flag.Int("consul-port", 8500, "server agent pb port")
+	consulAddr := flag.String("consul-addr", "127.0.0.1", "server agent ip address")
+	consulPort := flag.Int("consul-port", 8500, "server agent pb port")
 	id := flag.String("id", "none", "node id")
 	ip := flag.String("ip", "127.0.0.1", "my ip address")
 	port := flag.Int("port", 18888, "my ip address")
 	flag.Parse()
 
-	server.InitConsulClient(*id, *ip, *port, *cip, *cport)
+	server.InitConsulClient(*id, *ip, *port, *consulAddr, *consulPort)
 
 	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
